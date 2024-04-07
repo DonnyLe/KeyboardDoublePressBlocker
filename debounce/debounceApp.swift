bf6d8f98ec179df18a1837b3b2a7268426316ab1
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2024-04-07 02:07:29 -0400

    Added menu bar functionality
    Used MenuBarExtra and openWindow/dismissWindow Environment objects

--- debounce/debounceApp.swift
+++ debounce/debounceApp.swift
@@ -15,20 +15,41 @@ struct debounceApp: App {
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject var viewModel: ViewModel = ViewModel();
-
-
-    
-    
-
     var body: some Scene {
-        WindowGroup {
-            ContentView(appDel: appDelegate).environmentObject(viewModel)
+        MenuBarExtra("UtilityApp", systemImage: "hammer") {
+            KeyboardViewButton()
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
             }
-        
-          
         }
         
+        WindowGroup(id: "keyboard-view") {
+            ContentView(appDel: appDelegate).environmentObject(viewModel)
+        }
+                
     }
+    
+}
+
+struct KeyboardViewButton: View {
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+    @State var showWindow: Bool = false;
+
+
+    var body: some View {
+        Button("Settings") {
+            if(showWindow) {
+                dismissWindow(id: "keyboard-view")
+            } else {
+                dismissWindow(id: "keyboard-view")
+                openWindow(id: "keyboard-view")
+            }
+            self.showWindow = !self.showWindow;
+            
+        }
+    }
+}
     
 
 
